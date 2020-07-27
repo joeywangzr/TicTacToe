4c3b9d9666bf6decb746e96966f18df690220794
joeywangzr <joeywangzr@gmail.com>
2020-07-27 17:40:35 -0600

    fixed play again

--- tictactoe2.0.py
+++ tictactoe2.0.py
@@ -1,8 +1,7 @@
-# TO DO: CLEAR BOARD, p1 and p2 print.............
-
 import random
 
 print('Welcome to TicTacToe!')
+again = ''
 
 def print_board(board):
     print(board[1] + ' | ' + board[2] + ' | ' + board[3])
@@ -75,119 +74,74 @@ def clear(board):
     for i in [1,2,3,4,5,6,7,8,9]:
         board[i] = str(i)
 
-# Select number of players:
-while True:
-    try:
-        vs = int(input('How many players? (1 or 2): '))
-        if vs not in range(1,3):
-            print('Please input 1 or 2.')
-        else:
+def play_again():
+    while True:
+        global again
+        again = input('Play again? (y/n): ')
+        if again.lower() == 'y' or again.lower() == 'n':
             break
-    except ValueError:
-        print('Please input an integer.')
+        else:
+            print('Please input y or n.')
 
-if vs == 1:
-    # 1 Player:
-    board = ['#','1','2','3','4','5','6','7','8','9']
-    pa = True
-    while pa == True:
-        print_board(board)
-        player_one(board)
+while True:
+    # Select number of players:
+    while True:
+        try:
+            vs = int(input('How many players? (1 or 2): '))
+            if vs not in range(1,3):
+                print('Please input 1 or 2.')
+            else:
+                break
+        except ValueError:
+            print('Please input an integer.')
 
-        if check_win(board, 'X'):
+    if vs == 1:
+        # 1 Player:
+        board = ['#','1','2','3','4','5','6','7','8','9']
+        while True:
             print_board(board)
-            print('You win!')
-            again = input('Play again? (y/n): ')
-            while True:
-                if again.lower() == 'n':
-                    print('Thanks for playing.')
-                    pa = False
-                    break
-                elif again.lower() == 'y':
-                    clear(board)
-                    break
-                else: 
-                    print('Please input y or n.')
 
-        if check_win(board, 'O'):
+            if check_win(board, 'X'):
+                print_board(board)
+                print('You win!')
+                play_again()
+                break
+            if check_win(board, 'O'):
+                print_board(board)
+                print('You lose!')
+                play_again()
+                break
+            if check_tie(board):
+                print('Tie.')
+                play_again()
+                break
+
+            player_one(board)
+            comp(board)
+
+    else:
+        # 2 Players
+        board = ['#','1','2','3','4','5','6','7','8','9']
+        while True:
             print_board(board)
-            print('You lose!')
-            again = input('Play again? (y/n): ')
-            while True:
-                if again.lower() == 'n':
-                    print('Thanks for playing.')
-                    pa = False
-                    break
-                elif again.lower() == 'y':
-                    clear(board)
-                    break
-                else: 
-                    print('Please input y or n.')
-        if check_tie(board):
-            print('Tie.')
-            again = input('Play again? (y/n): ')
-            while True:
-                if again.lower() == 'n':
-                    print('Thanks for playing.')
-                    pa = False
-                    break
-                elif again.lower() == 'y':
-                    clear(board)
-                    break
-                else: 
-                    print('Please input y or n.')
-        comp(board)
 
-else:
-    # 2 Players
-    board = ['#','1','2','3','4','5','6','7','8','9']
-    pa = True
-    while pa == True:
-        print_board(board)
-        player_one(board)
-        print_board(board)
+            if check_win(board, 'X'):
+                print_board(board)
+                print('Player one wins!')
+                play_again()
+                break
+            if check_win(board, 'O'):
+                print_board(board)
+                print('Player 2 wins!')
+                play_again()
+                break
+            if check_tie(board):
+                print('Tie.')
+                play_again()
+                break
+                        
+            player_one(board)
+            player_two(board)
 
-        if check_win(board, 'X'):
-            print_board(board)
-            print('Player one wins!')
-            again = input('Play again? (y/n): ')
-            while True:
-                if again.lower() == 'n':
-                    print('Thanks for playing.')
-                    pa = False
-                    break
-                elif again.lower() == 'y':
-                    clear(board)
-                    break
-                else: 
-                    print('Please input y or n.')
-
-        if check_win(board, 'O'):
-            print_board(board)
-            print('Player 2 wins!')
-            again = input('Play again? (y/n): ')
-            while True:
-                if again.lower() == 'n':
-                    print('Thanks for playing.')
-                    pa = False
-                    break
-                elif again.lower() == 'y':
-                    clear(board)
-                    break
-                else: 
-                    print('Please input y or n.')
-        if check_tie(board):
-            print('Tie.')
-            again = input('Play again? (y/n): ')
-            while True:
-                if again.lower() == 'n':
-                    print('Thanks for playing.')
-                    pa = False
-                    break
-                elif again.lower() == 'y':
-                    clear(board)
-                    break
-                else: 
-                    print('Please input y or n.')
-
-        player_two(board)
+    if again == 'n':
+        break
